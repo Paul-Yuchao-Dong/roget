931b1c78478d32564f11609ed1d00a8e3c5187ff
kiki <paulcynic@gmail.com>
2023-02-12 08:16:04 -0500

    initial layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+const GAMES: &str = include_str!("../answers.txt");
 fn main() {
-    println!("Hello, world!");
+    let mut guesser = Naive::new();
+    for answer in GAMES.split_whitespace() {
+        play(answer, guesser);
+    }
+}
+
+fn play<G: Guesser>(answer: &'static str, guesser: G) {
+    // play rounds where it invoke guesser each time
+
+
+}
+
+enum Correctness {
+    ///Green
+    Correct,
+    ///Yellow
+    Misplaced,
+    ///Gray
+    Wrong,
+}
+
+struct Guess {
+    word: String,
+    mask: [Correctness; 5],
+
+}
+trait Guesser {
+    fn guess(&mut self, history: &[Guess]) -> String;
+}
+
+impl <T> Guesser for &mut T where T:Guesser{
+    fn guess(&mut self, history: &[Guess]) -> String;
 }
